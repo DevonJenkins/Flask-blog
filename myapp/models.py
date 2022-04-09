826d2d4356215f556f853e0a251978bd828167e1
DVNJNKNS <devonmjenkins3@gmail.com>
2022-04-08 21:10:15 -0400

    add blogpost model to models.py

--- myapp/models.py
+++ myapp/models.py
@@ -30,4 +30,20 @@ class User(db.Model, UserMixin):
     
     def __repr__(self):
         return f"Username {self.username}"
+        return f"Hash pass {self.password_hash}"
 
+class BlogPost(db.Model):
+    __tablename__ = 'blog_posts'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    title = db.Column(db.String(140), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __init__(self, title, text, user_id):
+        self.title = title 
+        self.text = text 
+        self.user_id = user_id
+    
+    def __repr__(self):
+        return f"Post Id: {self.id} -- Date: {self.date} --- Title: {self.title} "
